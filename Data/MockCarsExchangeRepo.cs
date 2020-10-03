65fdd185f5acf96171831314c2b8ebdf36d61170
Johel <johelcv@gmail.com>
2020-10-02 23:31:28 -0500

    CAmbio en Mock

--- Data/MockCarsExchangeRepo.cs
+++ Data/MockCarsExchangeRepo.cs
@@ -13,7 +13,7 @@ namespace CarsExchange.Data
                 new Car{Id=1,make="Audi",model="Spyder",year=2018},
                 new Car{Id=2,make="Ford",model="Mustang",year=2015},
                 new Car{Id=3,make="Ferrari",model="Portofino",year=2020},
-                new Car{Id=4,make="Aston Martin",model="DBS Superleggera",year=2017}
+                new Car{Id=4,make="Aston Martin",model="DBS Superleggera",year=2017},
                 new Car{Id=4,make="Jaguar",model="XKSS",year=1960}
             };
 
